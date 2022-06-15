1b1a7dbc62e24fc6bcdb06a2f37f9c64415ec9b2
matheustomaschewski <matheus_tomaschewski@hotmail.com>
2022-06-15 17:07:57 -0300

    final

--- ProjetoArduinoPoo2/Form1.Designer.cs
+++ ProjetoArduinoPoo2/Form1.Designer.cs
@@ -30,9 +30,9 @@ namespace ProjetoArduinoPoo2
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.gbConfig = new System.Windows.Forms.GroupBox();
             this.lbSerial = new System.Windows.Forms.Label();
             this.cbBoudRate = new System.Windows.Forms.ComboBox();
@@ -45,6 +45,10 @@ namespace ProjetoArduinoPoo2
             this.TimerSerial = new System.Windows.Forms.Timer(this.components);
             this.TimerLog = new System.Windows.Forms.Timer(this.components);
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.BtnGravar = new System.Windows.Forms.Button();
+            this.TxtLocal = new System.Windows.Forms.TextBox();
+            this.BtnLocal = new System.Windows.Forms.Button();
+            this.LbTempo = new System.Windows.Forms.Label();
             this.gbConfig.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chartLDR)).BeginInit();
             this.SuspendLayout();
@@ -53,6 +57,10 @@ namespace ProjetoArduinoPoo2
             // 
             this.gbConfig.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbConfig.Controls.Add(this.LbTempo);
+            this.gbConfig.Controls.Add(this.BtnLocal);
+            this.gbConfig.Controls.Add(this.TxtLocal);
+            this.gbConfig.Controls.Add(this.BtnGravar);
             this.gbConfig.Controls.Add(this.lbSerial);
             this.gbConfig.Controls.Add(this.cbBoudRate);
             this.gbConfig.Controls.Add(this.cbPortaCom);
@@ -64,7 +72,7 @@ namespace ProjetoArduinoPoo2
             this.gbConfig.Margin = new System.Windows.Forms.Padding(2);
             this.gbConfig.Name = "gbConfig";
             this.gbConfig.Padding = new System.Windows.Forms.Padding(2);
-            this.gbConfig.Size = new System.Drawing.Size(881, 167);
+            this.gbConfig.Size = new System.Drawing.Size(881, 171);
             this.gbConfig.TabIndex = 0;
             this.gbConfig.TabStop = false;
             this.gbConfig.Text = "Configurações";
@@ -72,7 +80,7 @@ namespace ProjetoArduinoPoo2
             // lbSerial
             // 
             this.lbSerial.AutoSize = true;
-            this.lbSerial.Location = new System.Drawing.Point(365, 128);
+            this.lbSerial.Location = new System.Drawing.Point(374, 60);
             this.lbSerial.Name = "lbSerial";
             this.lbSerial.Size = new System.Drawing.Size(134, 23);
             this.lbSerial.TabIndex = 6;
@@ -96,7 +104,7 @@ namespace ProjetoArduinoPoo2
             "38400",
             "57600",
             "115200"});
-            this.cbBoudRate.Location = new System.Drawing.Point(125, 106);
+            this.cbBoudRate.Location = new System.Drawing.Point(8, 128);
             this.cbBoudRate.Margin = new System.Windows.Forms.Padding(2);
             this.cbBoudRate.Name = "cbBoudRate";
             this.cbBoudRate.Size = new System.Drawing.Size(162, 31);
@@ -106,7 +114,7 @@ namespace ProjetoArduinoPoo2
             // 
             this.cbPortaCom.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbPortaCom.FormattingEnabled = true;
-            this.cbPortaCom.Location = new System.Drawing.Point(125, 42);
+            this.cbPortaCom.Location = new System.Drawing.Point(8, 70);
             this.cbPortaCom.Margin = new System.Windows.Forms.Padding(2);
             this.cbPortaCom.Name = "cbPortaCom";
             this.cbPortaCom.Size = new System.Drawing.Size(162, 31);
@@ -115,7 +123,7 @@ namespace ProjetoArduinoPoo2
             // lbBaudRate
             // 
             this.lbBaudRate.AutoSize = true;
-            this.lbBaudRate.Location = new System.Drawing.Point(4, 109);
+            this.lbBaudRate.Location = new System.Drawing.Point(4, 103);
             this.lbBaudRate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbBaudRate.Name = "lbBaudRate";
             this.lbBaudRate.Size = new System.Drawing.Size(109, 23);
@@ -134,10 +142,10 @@ namespace ProjetoArduinoPoo2
             // 
             // btnDesconectar
             // 
-            this.btnDesconectar.Location = new System.Drawing.Point(592, 58);
+            this.btnDesconectar.Location = new System.Drawing.Point(199, 86);
             this.btnDesconectar.Margin = new System.Windows.Forms.Padding(2);
             this.btnDesconectar.Name = "btnDesconectar";
-            this.btnDesconectar.Size = new System.Drawing.Size(219, 55);
+            this.btnDesconectar.Size = new System.Drawing.Size(161, 55);
             this.btnDesconectar.TabIndex = 1;
             this.btnDesconectar.Text = "Desconectar";
             this.btnDesconectar.UseVisualStyleBackColor = true;
@@ -145,10 +153,10 @@ namespace ProjetoArduinoPoo2
             // 
             // btnConectar
             // 
-            this.btnConectar.Location = new System.Drawing.Point(369, 58);
+            this.btnConectar.Location = new System.Drawing.Point(199, 27);
             this.btnConectar.Margin = new System.Windows.Forms.Padding(2);
             this.btnConectar.Name = "btnConectar";
-            this.btnConectar.Size = new System.Drawing.Size(219, 55);
+            this.btnConectar.Size = new System.Drawing.Size(161, 55);
             this.btnConectar.TabIndex = 0;
             this.btnConectar.Text = "Conectar";
             this.btnConectar.UseVisualStyleBackColor = true;
@@ -159,30 +167,71 @@ namespace ProjetoArduinoPoo2
             this.chartLDR.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            chartArea1.Name = "ChartArea1";
-            this.chartLDR.ChartAreas.Add(chartArea1);
-            legend1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            legend1.IsTextAutoFit = false;
-            legend1.Name = "Legend1";
-            this.chartLDR.Legends.Add(legend1);
-            this.chartLDR.Location = new System.Drawing.Point(12, 182);
+            chartArea2.Name = "ChartArea1";
+            this.chartLDR.ChartAreas.Add(chartArea2);
+            legend2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            legend2.IsTextAutoFit = false;
+            legend2.Name = "Legend1";
+            this.chartLDR.Legends.Add(legend2);
+            this.chartLDR.Location = new System.Drawing.Point(12, 186);
             this.chartLDR.Name = "chartLDR";
-            series1.BorderWidth = 10;
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            series1.LabelBorderWidth = 10;
-            series1.Legend = "Legend1";
-            series1.MarkerBorderWidth = 5;
-            series1.Name = "LDR";
-            this.chartLDR.Series.Add(series1);
-            this.chartLDR.Size = new System.Drawing.Size(881, 522);
+            series2.BorderWidth = 10;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series2.LabelBorderWidth = 10;
+            series2.Legend = "Legend1";
+            series2.MarkerBorderWidth = 5;
+            series2.Name = "LDR";
+            this.chartLDR.Series.Add(series2);
+            this.chartLDR.Size = new System.Drawing.Size(881, 518);
             this.chartLDR.TabIndex = 1;
             this.chartLDR.Text = "chart1";
             // 
             // TimerSerial
             // 
             this.TimerSerial.Tick += new System.EventHandler(this.TimerSerial_Tick);
+            // 
+            // TimerLog
+            // 
+            this.TimerLog.Tick += new System.EventHandler(this.TimerLog_Tick);
+            // 
+            // BtnGravar
+            // 
+            this.BtnGravar.Location = new System.Drawing.Point(715, 86);
+            this.BtnGravar.Name = "BtnGravar";
+            this.BtnGravar.Size = new System.Drawing.Size(161, 55);
+            this.BtnGravar.TabIndex = 7;
+            this.BtnGravar.Text = "Gravar";
+            this.BtnGravar.UseVisualStyleBackColor = true;
+            this.BtnGravar.Click += new System.EventHandler(this.BtnGravar_Click);
+            // 
+            // TxtLocal
+            // 
+            this.TxtLocal.Location = new System.Drawing.Point(378, 27);
+            this.TxtLocal.Name = "TxtLocal";
+            this.TxtLocal.ReadOnly = true;
+            this.TxtLocal.Size = new System.Drawing.Size(426, 30);
+            this.TxtLocal.TabIndex = 8;
+            // 
+            // BtnLocal
+            // 
+            this.BtnLocal.Location = new System.Drawing.Point(810, 26);
+            this.BtnLocal.Name = "BtnLocal";
+            this.BtnLocal.Size = new System.Drawing.Size(66, 30);
+            this.BtnLocal.TabIndex = 9;
+            this.BtnLocal.Text = "Local";
+            this.BtnLocal.UseVisualStyleBackColor = true;
+            this.BtnLocal.Click += new System.EventHandler(this.BtnLocal_Click);
+            // 
+            // LbTempo
+            // 
+            this.LbTempo.AutoSize = true;
+            this.LbTempo.Location = new System.Drawing.Point(374, 103);
+            this.LbTempo.Name = "LbTempo";
+            this.LbTempo.Size = new System.Drawing.Size(331, 23);
+            this.LbTempo.TabIndex = 10;
+            this.LbTempo.Text = "Tempo salvar dados: [não calculado]";
             // 
             // Form1
             // 
@@ -220,6 +269,10 @@ namespace ProjetoArduinoPoo2
         private System.Windows.Forms.Timer TimerLog;
         private System.Windows.Forms.Label lbSerial;
         private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button BtnGravar;
+        private System.Windows.Forms.TextBox TxtLocal;
+        private System.Windows.Forms.Button BtnLocal;
+        private System.Windows.Forms.Label LbTempo;
     }
 }
 
--- ProjetoArduinoPoo2/Form1.cs
+++ ProjetoArduinoPoo2/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO.Ports;
+using System.IO;
+using System.Diagnostics;
 
 namespace ProjetoArduinoPoo2
 {
@@ -15,6 +17,7 @@ namespace ProjetoArduinoPoo2
     {
 
         SerialPort serialPort = new SerialPort();
+        int intervalo = 100;
 
         public Form1()
         {
@@ -24,6 +27,7 @@ namespace ProjetoArduinoPoo2
         private void Form1_Load(object sender, EventArgs e)
         {
             btnDesconectar.Enabled = false;
+            BtnGravar.Enabled = false;
 
             cbPortaCom.Items.AddRange(SerialPort.GetPortNames());
 
@@ -34,9 +38,18 @@ namespace ProjetoArduinoPoo2
 
             cbBoudRate.SelectedIndex = 5;
 
-            //chartLDR.ChartAreas[0].AxisX.Maximum = 30;
-            chartLDR.ChartAreas[0].AxisY.Minimum = -10;
-            chartLDR.ChartAreas[0].AxisY.Maximum = 1200;
+            for(int i = 0; i == 30; i++)
+            {
+                string hora = DateTime.Now.ToLongTimeString();
+                chartLDR.Series[0].Points.AddXY(hora, 0);
+                if(chartLDR.Series[0].Points.Count == 50)
+                {
+                    chartLDR.Series[0].Points.RemoveAt(0);
+                }
+            }
+
+            chartLDR.ChartAreas[0].AxisY.Minimum = 850;
+            chartLDR.ChartAreas[0].AxisY.Maximum = 1100;
 
         }
 
@@ -48,27 +61,39 @@ namespace ProjetoArduinoPoo2
                 serialPort.Close();
                 btnDesconectar.Enabled = false;
                 btnConectar.Enabled = true;
+                BtnGravar.Enabled = false;
                 return;
             }
             else
             {
-                btnDesconectar.Enabled = true;
-                btnConectar.Enabled = false;
-
-                serialPort.PortName = cbPortaCom.Text;
-                serialPort.BaudRate = Int32.Parse(cbBoudRate.Text);
-
-                try
+                if(!(cbPortaCom.SelectedIndex > -1) || !(cbBoudRate.SelectedIndex > -1))
                 {
-                    serialPort.Open();
-                    TimerSerial.Interval = 1000;
-                    TimerSerial.Start();
+                    return;
                 }
-                catch (Exception ex)
+                else
                 {
-                    MessageBox.Show(ex.Message);
 
-                }
+                    btnDesconectar.Enabled = true;
+                    btnConectar.Enabled = false;
+                    //BtnGravar.Enabled = true;
+
+                    serialPort.PortName = cbPortaCom.Text;
+                    serialPort.BaudRate = Int32.Parse(cbBoudRate.Text);
+
+                    try
+                    {
+                        chartLDR.Series[0].Points.Clear();
+
+                        serialPort.Open();
+                        TimerSerial.Interval = intervalo;
+                        TimerSerial.Start();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+
+                    }
+                }                
             }  
         }
 
@@ -79,28 +104,141 @@ namespace ProjetoArduinoPoo2
                 serialPort.Close();
                 btnConectar.Enabled = true;
                 btnDesconectar.Enabled = false;
+                BtnGravar.Enabled = false;
 
                 TimerSerial.Stop();
+                TimerLog.Stop();
+
+                serialPort.Close();
             }
         }
 
-        int i = 0;
+        string serialRam;
+
         private void TimerSerial_Tick(object sender, EventArgs e)
         {
-            string serialIn = serialPort.ReadLine();
-            string serialRam;
+
+            var serialIn = serialPort.ReadLine();
+            
 
             TextBox tb = new TextBox();
             tb.Multiline = true;
             tb.Text = serialIn;
 
-            serialRam = tb.Lines[0];
+            if(tb.Lines.Length > 0)
+            {
+                serialRam = tb.Lines[0];
 
-            lbSerial.Text = "LDR (valor): " + serialRam;
+                lbSerial.Text = "LDR (valor): " + serialRam;
 
-            i++;
-            chartLDR.Series[0].Points.AddXY(i, serialRam);
+                //string hora = DateTime.Now.ToLongTimeString();
+                //chartLDR.Series[0].Points.AddXY(hora, serialRam);
+
+                string hora = DateTime.Now.ToLongTimeString();
+                chartLDR.Series[0].Points.AddXY(hora, serialRam);
+                if (chartLDR.Series[0].Points.Count == 50)
+                {
+                    chartLDR.Series[0].Points.RemoveAt(0);
+                }
+
+            }
+            
+
+            //SalvarNoTxt(serialRam);
 
         }
+
+        private async Task SalvarNoTxt(string Dados)
+        {
+
+            Stopwatch crono = new Stopwatch();
+            crono.Start();
+
+            string dataHora = DateTime.Now.ToString();
+            string texto = $"[{dataHora}] - {Dados}";
+
+            string caminho = Path.Combine(TxtLocal.Text, "DadosLidos.txt");
+
+            StreamWriter arquivo = new StreamWriter(caminho, true);
+
+            if (File.Exists(caminho))
+            {
+                try
+                {
+                    await arquivo.WriteLineAsync(texto);
+                    //arquivo.Close();
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message);
+                }
+                finally { arquivo.Close(); }
+            }
+            else
+            {
+                try
+                {
+                    arquivo = File.CreateText(caminho);
+                    await arquivo.WriteLineAsync(texto);
+                    //arquivo.Close();
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message);
+                }
+                finally { arquivo.Close(); }
+                
+            }
+
+            crono.Stop();
+            LbTempo.Text = $"Tempo salvar dados: {crono.Elapsed.TotalMilliseconds} ms";
+
+            //StreamWriter arquivo = new StreamWriter(caminho);
+            //arquivo.WriteLine(texto);
+
+        }
+
+        private void BtnLocal_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fdl = new FolderBrowserDialog();
+            DialogResult result = fdl.ShowDialog();
+
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fdl.SelectedPath))
+            {
+                TxtLocal.Text = fdl.SelectedPath.ToString();
+                BtnGravar.Enabled = true;
+            }
+
+        }
+
+        private void BtnGravar_Click(object sender, EventArgs e)
+        {
+            if(TxtLocal.TextLength == 0)
+            {
+                MessageBox.Show("Por favor clique no botão 'Local' e selecione um local para salvar o arquivo Txt.");
+                BtnGravar.Enabled = false;
+            }
+            else
+            {
+                if(serialPort.IsOpen)
+                {
+                    TimerLog.Interval = intervalo;
+                    TimerLog.Start();
+                    BtnGravar.Enabled = false;
+                }
+            }
+        }
+
+        private async void TimerLog_Tick(object sender, EventArgs e)
+        {
+            await SalvarNoTxt(serialRam);
+        }
+
+        //private async Task MetodoSalvar(string dados)
+        //{
+        //    await SalvarNoTxt(dados);
+        //}
     }
 }
